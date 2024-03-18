c8751930a257e64ec47ab454e10f6c16fba78e23
mukesh292004 <mukeshnkl2004@gmail.com>
2024-03-18 12:21:32 +0000

    change

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -31,6 +31,7 @@ export default function NavLinks() {
           <Link
           key={link.name}
           href={link.href}
+          // asdfasdf
           className={clsx(
             'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
             {
